ae971781180bc557ce1f2c5f6e77b7c232a64566
Mayy isme <3hung1998@gmail.com>
2023-11-14 21:56:13 +0700

    source day14

--- tesst.js
+++ tesst.js
@@ -1,4 +1,65 @@
-console.log("Hello world from HTML");
-setTimeout(() => {
-  console.log("Hello sau 3s");
-}, 3000);
+//--HTTP Request--
+
+// let request = new XMLHttpRequest();
+
+// request.onreadystatechange = function () {
+//   if (this.readyState === 4) {
+//     console.log(">>> chekc res", request);
+//   }
+// };
+
+// request.open("GET", "https://jsonplacehilde,typicode.com.todos", true);
+// request.send();
+
+// -- Callback Functions--
+
+// function getTodos() {
+//   let request = new XMLHttpRequest();
+
+//   request.onreadystatechange = function () {
+//     if ((this.readyState === 4 && this, request.status === 200)) {
+//       console.log(">>> chekc res", request);
+//     } else {
+//       console.log(">>status:", request.status);
+//     }
+//   };
+
+//   request.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+//   request.send();
+// }
+
+// getTodos();
+
+// -- Callback Functions VD thêm--
+
+const callback = (error, data) => {
+  if (error) {
+    console.log(">> calling callback with error: ", error);
+  }
+  if (data) {
+    console.log(">> calling callback with data: ", data);
+  }
+};
+
+function getTodos() {
+  let request = new XMLHttpRequest();
+
+  request.onreadystatechange = function () {
+    if ((this.readyState === 4 && this, request.status === 200)) {
+      // Lấy data tu ben tab Netwwork va Convert JSON
+      const data = JSON.parse(request.responseText);
+      //--Convert JSON -> string
+      const datatring = JSON.stringify(data);
+      callback(undefined, data);
+      callback(undefined, datatring);
+    }
+    if ((this.readyState === 4 && this, request.status !== 200)) {
+      callback("somethings wrongs", undefined);
+    }
+  };
+
+  request.open("GET", "https://jsonplaceholder.typicode.com/todos", true);
+  request.send();
+}
+
+getTodos(callback);
